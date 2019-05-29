d3304c64cfc6685400d2a90f17d77fe20a77db07
Ivan <ivan-my-mail@yandex.com>
2019-05-29 21:57:24 +0300

    Added COM-port support (part. #1)
    Added COM-port support from reverse-engeneering Alex Guyver's executable of OpenHardwareMonitor with COM-port support that hasn't open source code

---
+++ Properties.cs
@@ -0,0 +1,52 @@
+﻿using System.CodeDom.Compiler;
+using System.Configuration;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
+namespace OpenHardwareMonitor
+{
+	[CompilerGenerated]
+	[GeneratedCode("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "12.0.0.0")]
+	internal sealed class Properties : ApplicationSettingsBase
+	{
+		private static Properties defaultInstance = (Properties)SettingsBase.Synchronized((SettingsBase)new Properties());
+
+		public static Properties Default
+		{
+			get
+			{
+				return defaultInstance;
+			}
+		}
+
+		[UserScopedSetting]
+		[DebuggerNonUserCode]
+		[DefaultSettingValue("COM3")]
+		public string ComName
+		{
+			get
+			{
+				return (string)this[nameof(ComName)];
+			}
+			set
+			{
+				this[nameof(ComName)] = (object)value;
+			}
+		}
+
+		[UserScopedSetting]
+		[DebuggerNonUserCode]
+		[DefaultSettingValue("9600")]
+		public int BaudRate
+		{
+			get
+			{
+				return (int)this[nameof(BaudRate)];
+			}
+			set
+			{
+				this[nameof(BaudRate)] = (object)value;
+			}
+		}
+	}
+}
---
+++ Utilities/Serial.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.IO;
+using System.IO.Ports;
+using System.Windows.Forms;
+
+namespace OpenHardwareMonitor.Utilities
+{
+	public class Serial
+	{
+		private string _Port;
+		private SerialPort serial;
+
+		public string Port
+		{
+			get
+			{
+				return this._Port;
+			}
+			set
+			{
+				this._Port = value;
+				Properties.Default.ComName = this._Port;
+				Properties.Default.Save();
+			}
+		}
+
+		public Serial()
+		{
+			int baudRate = Properties.Default.BaudRate;
+			this.Port = Properties.Default.ComName;
+			this.serial = new SerialPort(string.IsNullOrEmpty(this.Port) ? "COM1" : this.Port, baudRate);
+		}
+
+		public bool Open()
+		{
+			if (this.serial.IsOpen)
+			{
+				try
+				{
+					this.serial.Close();
+				}
+				catch
+				{
+				}
+			}
+			this.serial.PortName = this.Port;
+			try
+			{
+				this.serial.Open();
+			}
+			catch (IOException ex)
+			{
+				return false;
+			}
+			return true;
+		}
+
+		public void Close()
+		{
+			if (!this.serial.IsOpen)
+				return;
+			this.serial.Close();
+		}
+
+		public void Write(byte[] data)
+		{
+			if (!this.serial.IsOpen)
+				return;
+			try
+			{
+				this.serial.Write(data, 0, data.Length);
+			}
+			catch (Exception ex)
+			{
+				int num = (int)MessageBox.Show("Ошибка отправки данных в COM-порт:\r\n\r\n" + ex.Message);
+				try
+				{
+					this.serial.Close();
+				}
+				catch
+				{
+				}
+				this.Open();
+			}
+		}
+	}
+}
